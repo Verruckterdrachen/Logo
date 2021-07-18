c5f408355812c6001742dbc88f0b02cb246918a3
verruckterdrachen <verruckterdrachen@gmail.com>
2021-07-18 15:42:13 +0300

    feat: add search form

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -9,3 +9,33 @@ for (let index = 0; index < menuParents.length; index++) {
     menuParent.classList.remove('_active');
   });
 }
+
+let menuPageBurger = document.querySelector('.menu-page__burger');
+let menuPageBody = document.querySelector('.menu-page__body');
+menuPageBurger.addEventListener("click", function(e) {
+  menuPageBurger.classList.toggle('_active');
+  _slideToggle(menuPageBody);
+});
+
+let searchSelect = document.querySelector('.search-page__title');
+let categoriesSearch = document.querySelector('.categories-search');
+searchSelect.addEventListener("click", function(e) {
+  searchSelect.classList.toggle('_active');
+  _slideToggle(categoriesSearch);
+});
+
+let checkboxCategories = document.querySelectorAll('.categories-search__checkbox');
+for (let index = 0; index < checkboxCategories.length; index++) {
+  const checkboxCategory = checkboxCategories[index];
+  checkboxCategory.addEventListener("change", function(e) {
+    checkboxCategory.classList.toggle('_active');
+    const checkboxActiveCategories = document.querySelectorAll('.categories-search__checkbox._active');
+    if(checkboxActiveCategories.length > 0) {
+      searchSelect.classList.add('_categories');
+      let searchQuantity = searchSelect.querySelector('.search-page__quantity');
+      searchQuantity.innerHTML = searchQuantity.getAttribute('data-text') + ' ' + checkboxActiveCategories.length;
+    } else {
+      searchSelect.classList.remove('_categories');
+    }
+  });
+}
